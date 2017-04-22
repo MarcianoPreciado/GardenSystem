c98edf93e1037277057d1b6eed985a88cc2f3773
MarcianoPreciado <marcianocpreciado@gmail.com>
2017-04-22 16:56:21 -0600

    Add CommLink Class

--- Microcontroller/Lib/GardenSystem/src/comm_link.h
+++ Microcontroller/Lib/GardenSystem/src/comm_link.h
@@ -3,21 +3,31 @@
 
 #include <Arduino.h>
 // Include Arduino Json Interpreter
-#include <ArduinoJson>
+#include <ArduinoJson.h>
+
+#include "garden_cell.h"
+#include "garden_system.h"
 
 class CommLink {
 public:
-  CommLink()
-    { Serial.begin(9600); }
+  static void Begin(int baud, GardenSystem *pg_s);
+  static void Update();
 
-  void Update();
   // Data outgoing
-  void SendSensorData(Sensor s);
+  static void Error(String err_mes);
+  static void SendCellSensorInfo(const GardenCell &rg_c);
   // Data incoming
+  static void Interpret(JsonObject &root);
 
 private:
+  static const uint16_t buf_size = 120;
+  static GardenSystem *pg_s_;
+  static void PlantOperation(JsonObject &root);
+  static void CellOperation(JsonObject &root);
+  static void SystemOperation(JsonObject &root);
 
 };
 
-extern CommLink Comm;
 #endif
+//{"kind":"cell","op":"remove","cell":4}{"kind":"plant","op":"remove","cell":3,"position":2}
+//{"kind":"cell","op":"add","num_valves":6,"spins":[2,3,4,5,6,7],"flowrate":[1.0,0.5,0.5,1.0,0.5,0.5],"lpin":8,"cell":3}
