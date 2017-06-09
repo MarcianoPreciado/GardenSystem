0b662c4ed70a14cb78f04775626e3283c340ab00
Marciano Preciado <marcianocpreciado@gmail.com>
2017-06-08 18:52:40 -0600

    Example Fix
    int -> uin8_t

--- Microcontroller/Lib/GardenSystem/src/components/lights.h
+++ Microcontroller/Lib/GardenSystem/src/components/lights.h
@@ -7,28 +7,38 @@
 class Lights{
 public:
   // Constructor/Destructor
-  Lights(int pin)
-    { pin_ = pin; pinMode(pin_, OUTPUT); }
+  Lights(uint8_t on_pin, uint8_t off_pin)
+    { on_pin_ = on_pin; pinMode(on_pin_, OUTPUT);
+      off_pin_ = off_pin; pinMode(off_pin_, OUTPUT);}
   ~Lights(){}
 
   // Getters/Setters
   bool is_active()
     { return is_active_; }
-  int get_pin()
-    { return pin_; }
-  void set_pin(int pin)
-    { pin_ = pin; }
+  int get_on_pin()
+    { return on_pin_; }
+  void set_on_pin(int pin)
+    { on_pin_ = pin; }
+  int get_off_pin()
+    { return off_pin_; }
+  void set_off_pin(int pin)
+    { off_pin_ = pin; }
 
   // Critical Functions TODO: Fix for actual lights
   void Activate()
-    { digitalWrite(pin_, HIGH); is_active_ = true; }
+    { TripPin(on_pin_); is_active_ = true; }
   void Deactivate()
-    { digitalWrite(pin_, LOW); is_active_ = false; }
+    { TripPin(off_pin_); is_active_ = false; }
 
 private:
-  int pin_;
+  uint8_t on_pin_;
+  uint8_t off_pin_;
   bool is_active_;
-
+  TripPin(uint8_t pin){
+    digitalWrite(pin, HIGH);
+    delay(20);
+    digitalWrite(pin, LOW);
+  }
 };
 
 #endif
